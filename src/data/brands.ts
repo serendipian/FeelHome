6d541c4e8502bb359c775d0f15f12f59e56efdc7
serendipian <serendipian@users.noreply.github.com>
2026-03-17 02:18:17 +0000

    feat: Add local brand logo images and update brand data paths to use them.

--- src/data/brands.ts
+++ src/data/brands.ts
@@ -7,7 +7,7 @@ export const brands: Record<string, Brand> = {
     shortName: 'FH',
     description: 'Rental Agency',
     color: '#d4875a',
-    logo: 'https://media.licdn.com/dms/image/v2/D4E0BAQGiSdDJ-We0Ig/company-logo_200_200/company-logo_200_200/0/1712537881210/feel_home_morocco_logo?e=2147483647&v=beta&t=mxKEy_sdp8-RhtNyMyAxF_c5ZaxOeXmkt8SRzGv92F8',
+    logo: '/logos/feelhome.jpg',
     initials: 'FH',
   },
   mInvest: {
@@ -24,7 +24,7 @@ export const brands: Record<string, Brand> = {
     shortName: 'EX',
     description: 'Media Platform',
     color: '#1d7ff3',
-    logo: 'https://media.licdn.com/dms/image/v2/D4E0BAQF_FPzVxm3tMw/company-logo_100_100/company-logo_100_100/0/1733782692982/top_web3_jobs_logo?e=1774483200&v=beta&t=WaF_fN2IdzylPxHBjhzDaWhwzZqQMTNzw54-HndHup8',
+    logo: '/logos/expats.jpg',
     initials: 'EX',
   },
 };
